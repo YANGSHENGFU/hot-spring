d2932741f2e99ca0d2bee1273ed3234f5d5c92e7
xiahong <xihongmail@126.com>
2019-07-01 10:36:00 +0800

    夏洪测试

--- hotspr/src/main/java/com/hotspr/app/HotSpringApp.java
+++ hotspr/src/main/java/com/hotspr/app/HotSpringApp.java
@@ -13,6 +13,8 @@ public class HotSpringApp extends Application {
         CrashHandler.getInstance().init(getApplicationContext());
 
 
+
+
 //        XLogHelper.initXlog(getApplicationContext());
     }
 }
